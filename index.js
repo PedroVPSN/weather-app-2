1dea958e1c345d7de6bdfd5ef66a5f986afed2b3
Pedro Silva <pedrosilvavpsn@gmail.com>
2020-02-21 15:48:20 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+//import handlebars and path
+const hbs = require('express-handlebars');
+const path = require('path');
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+//Tell express to use this folder
+//Tell express that this path is static content for the client
+//Use path to join these tow paths
+
+const getWeather = require("./lib/getWeather"); // Import the getWeather function
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.engine('.hbs', hbs({
+    defaultLayout: 'layout',
+    extname: '.hbs' 
+}));
+
+app.set('view engine', '.hbs');
+
+app.get('/', (req, res) => {   
+    res.render('index');
+});
+
+app.get('/about', (req, res) =>{
+    res.render('about')
+});
+
+app.get('/wind', (req, res) =>{
+    // let location = req.body.location;
+    // let data = await getWeather(location);
+    // let wind = data.wind.speed;
+    res.render('windspeed');
+});
+
+app.post('/wind', async(req, res) => {
+    let location = req.body.location;
+    console.log(location)
+    let data = await getWeather(location); // data will be the weather information
+    let wind = data.wind.speed;
+    res.render('windspeed', {data: {location, wind}});
+});
+
+app.post('/', async(req, res) => {
+    let location = req.body.location;
+    console.log(location)
+    let data = await getWeather(location); // data will be the weather information
+    let temp = `${Math.ceil((data.main.temp))} °C`;
+    let humidity = data.main.humidity;
+    let condition = data.weather[0].description;
+    let feels = `${Math.ceil((data.main.feels_like))} °C`;
+    let weatherIcon = data.weather[0].icon;
+    let iconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+    
+    // let temp = `${Math.floor((data.main.temp))} °C`
+    // let loc = `${(data.name)}`
+    // res.render('index', {temp, loc, cond});
+    // return data.main.temp
+    res.render('index', {data: {location, temp, humidity, condition, feels}, iconUrl});
+});
+
+
+app.listen(3000, () => {
+    console.log('server listening on port 3000');
+});
